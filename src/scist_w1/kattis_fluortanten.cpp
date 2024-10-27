0ed5f9b05ebef9978da5d6fa671061df52904458
algasami <105358814+algasami@users.noreply.github.com>
2024-10-27 19:08:22 +0800

    [AC] kattis fluortanten

--- src/scist_w1/kattis_fluortanten.cpp
+++ src/scist_w1/kattis_fluortanten.cpp
@@ -5,16 +5,17 @@
 
 using namespace std;
 
+// suffix sum: opposite of prefix sum!
+// use long long to prevent accidental overflow >:(
+long long int suffix_sum[MAXSIZE + 1], weight[MAXSIZE];
 int main()
 {
 	int n;
-	int weightsum = 0, realsum = 0;
-	int prefix_sum[MAXSIZE + 1], weight[MAXSIZE];
+	long long int weightsum = 0, realsum = 0;
 	scanf("%d", &n);
-	prefix_sum[0] = 0;
 	for (int i = 0; i < n; i++)
 	{
-		scanf("%d", weight + i);
+		scanf("%lld", weight + i);
 		if (weight[i] == 0)
 		{
 			n--;
@@ -23,18 +24,21 @@ int main()
 		}
 		weightsum += weight[i];
 		realsum += (i + 1) * weight[i];
-		prefix_sum[i + 1] = prefix_sum[i] + weight[i];
 	}
-
-	int max_weight_delt = 0;
+	suffix_sum[n] = 0;
 	for (int i = n - 1; i >= 0; i--)
 	{
-		int happiness_delta = weightsum - prefix_sum[i];
-		if (happiness_delta > max_weight_delt)
+		suffix_sum[i] = suffix_sum[i + 1] + weight[i];
+	}
+
+	long long int max_weight_delt = 0;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (suffix_sum[i] > max_weight_delt)
 		{
-			max_weight_delt = happiness_delta;
+			max_weight_delt = suffix_sum[i];
 		}
 	}
-	printf("%d\n", max_weight_delt + realsum);
+	printf("%lld\n", max_weight_delt + realsum);
 	return 0;
 }
