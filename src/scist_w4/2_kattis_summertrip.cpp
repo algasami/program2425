ab1a90033be59db7752aee683edb9bfa9bc88320
algasami <105358814+algasami@users.noreply.github.com>
2025-01-28 11:02:48 +0800

    AC 2

--- src/scist_w4/2_kattis_summertrip.cpp
+++ src/scist_w4/2_kattis_summertrip.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 // #define DEBUG
 
-int event_psum[26][100005] = {}; // prefix sum for events
+int ucount[26] =
+    {}; // when ending with character C, the total unique combination currently is ucount[C].
+unsigned long long had_prev[26][26] =
+    {}; // had_prev[A][B] = When encountering character A, we have seen B (aka B is BEFORE A)
 
 int main()
 {
@@ -16,42 +19,31 @@ int main()
     getline(cin, s);
 
     size_t len = s.size();
+    unsigned long long good_trip = 0;
     for (size_t i = 0; i < len; i++)
     {
-        event_psum[s[i] - 'a'][i]++;
+        size_t ch = s[i] - 'a';
+        good_trip += ucount[ch];
+        ucount[ch] = 0; // reset unique combination (we've registered)
+        memset(had_prev[ch], 0,
+               sizeof(had_prev[ch])); // reset everything for ch being the start (because we
+                                      // just saw B = jettisoned ucount)
+        // ^ This make sure strings like `aab` only count for the second a because it is unique
         for (size_t j = 0; j < 26; j++)
         {
-            event_psum[j][i + 1] = event_psum[j][i];
-        }
-    }
-
-    unsigned long long good_trip = 0;
-    for (size_t i = 0; i < len - 1; i++)
-    {
-        size_t start_ch = s[i] - 'a';
-        for (size_t j = i + 1; j - i <= 26 && j < len; j++)
-        {
-            size_t end_ch = s[j] - 'a';
-            if (start_ch == end_ch)
+            if (j == ch)
             {
                 continue;
             }
-            size_t start_inc = event_psum[start_ch][j] - event_psum[start_ch][i];
-            size_t end_inc = event_psum[end_ch][j] - event_psum[end_ch][i];
-#ifdef DEBUG
-            cout << start_inc << ' ' << end_inc << '\n';
-            cout << string_view(s).substr(i, j - i + 1) << '\n';
-#endif
-            if (start_inc > 0)
+            if (!had_prev[j][ch]) // haven't seen ch yet (unique)
             {
-                break;
-            }
-            if (start_inc == 0 && end_inc == 1)
-            {
-                good_trip++;
+                had_prev[j][ch] = 1;
+                ucount[j]++; // should we encounter j in the future, we will get the unique combo
+                // uncertainty = cache
             }
         }
     }
+
     cout << good_trip << '\n';
 
     return 0;
